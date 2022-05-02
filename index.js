5d9ba0d29e4db996fc6aba323580e1401cb24af6
Hughes Mugera <hughes.mugera@touchinspiration.com>
2022-05-02 21:10:29 +0300

    commit tree e398c593f98b63b81b370bddeb0bd2ebe5ae7a77

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+// const returnFirstTwoDrivers = function (array) {
+//   let res = array.splice(0, 2);
+//   return res;
+// };
+// const returnLastTwoDrivers = function (array) {
+//   let res = array.splice(-2);
+//   return res;
+// };
+const returnFirstTwoDrivers = function (array) {
+  return array.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (array) {
+  return array.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (value) {
+  return function (multipliervalue) {
+    return value * multipliervalue;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+// const fareDoubler = (value) => {
+//   return value * 2;
+// };
+
+// const fareTripler = (value) => {
+//   return value * 3;
+// };
+
+const selectDifferentDrivers = function (arrayOfDrivers, func) {
+  return func(arrayOfDrivers);
+};
+
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+console.log(returnLastTwoDrivers(drivers));
